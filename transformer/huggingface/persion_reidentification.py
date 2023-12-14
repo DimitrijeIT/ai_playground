10ca6e092029d6c400bb528492335677b576ec2b
Dimitrije Stojanovic <dimitrije.it@gmail.com>
2023-12-14 14:47:39 +0100

    Add reidentification example

---
+++ transformer/huggingface/persion_reidentification.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+from transformers import pipeline
+import matplotlib.pyplot as plt
+import numpy as np
+import torch   
+from torch import nn
+from helpers import *
+
+
